7996ad7a0fe5bcc8f71f14b236988acad2e750f1
Andres Martin <andresmartin8787@gmail.com>
2012-05-16 18:50:22 +0200

    added bundles for i18n and translate from JSM

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new JMS\I18nRoutingBundle\JMSI18nRoutingBundle(),
+            new JMS\TranslationBundle\JMSTranslationBundle(),
             new PFCD\TourismBundle\PFCDTourismBundle(),
         );
 
