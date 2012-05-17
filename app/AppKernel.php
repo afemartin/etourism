1ab5aa0e09f08fecb84267df745a451bf026acc6
Andres Martin <andresmartin8787@gmail.com>
2012-05-17 21:40:30 +0200

    removed JMSTranslationBundle and PHPParser

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new JMS\I18nRoutingBundle\JMSI18nRoutingBundle(),
-            new JMS\TranslationBundle\JMSTranslationBundle(),
             new PFCD\TourismBundle\PFCDTourismBundle(),
         );
 
