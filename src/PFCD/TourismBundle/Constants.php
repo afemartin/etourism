b99307f95e11800d0af13fc3a375bea9b898c28c
Andres Martin <andresmartin8787@gmail.com>
2012-07-01 18:47:12 +0200

    added numeric day of the week constants

--- src/PFCD/TourismBundle/Constants.php
+++ src/PFCD/TourismBundle/Constants.php
@@ -14,6 +14,14 @@ class Constants
     const ORGANIZATION = 'organization';
     const ACTIVITY     = 'activity';
     const NEWS         = 'news';
+    
+    const MONDAY    = 1;
+    const TUESDAY   = 2;
+    const WEDNESDAY = 3;
+    const THURSDAY  = 4;
+    const FRIDAY    = 5;
+    const SATURDAY  = 6;
+    const SUNDAY    = 7;
 }
 
 ?>
