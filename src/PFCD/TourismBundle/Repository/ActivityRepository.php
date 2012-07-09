ec85a4e6d86d6710f06dac2ef121b21825f70e6d
Andres Martin <andresmartin8787@gmail.com>
2012-07-09 21:30:43 +0200

    moved 'findAvailability()' function from ActivityRepository to SessionRepository

--- src/PFCD/TourismBundle/Repository/ActivityRepository.php
+++ src/PFCD/TourismBundle/Repository/ActivityRepository.php
@@ -4,6 +4,9 @@ namespace PFCD\TourismBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
 
+use PFCD\TourismBundle\Entity\Session;
+use PFCD\TourismBundle\Entity\Reservation;
+
 use \DateTime;
 use \DatePeriod;
 use \DateInterval;
@@ -11,48 +14,6 @@ use \DateInterval;
 class ActivityRepository extends EntityRepository
 {
 
-    const CALENDAR_DAYS_INTERVAL = 13;
-    const CALENDAR_DAYS_SPAN = 14;
-    const DATE_FORMAT = "Y-m-d";
-
-    public function findAvailability($dateStart, $capacity, $daysweek)
-    {
-        // if not initial date or start date already was before set today as start date
-        if (!$dateStart || $dateStart < new DateTime())
-        {
-            $dateStart = new DateTime();
-        }
-
-        // adjust the interval to full weeks from Monday to Sunday
-        $dateStart = new DateTime(date(self::DATE_FORMAT, strtotime('Monday this week', $dateStart->getTimestamp())));
-        $dateEnd = clone $dateStart;
-        $dateEnd->add(new DateInterval('P' . self::CALENDAR_DAYS_INTERVAL . 'DT23H59M59S'));
-
-        $interval = DateInterval::createFromDateString('1 day');
-        $days = new DatePeriod($dateStart, $interval, $dateEnd);
-        
-        $calendar = array();
-        $calendar['years'][$dateStart->format("Y")] = 0;
-        $calendar['months'][$dateStart->format("F")] = 0;
-        $calendar['years'][$dateEnd->format("Y")] = 0;
-        $calendar['months'][$dateEnd->format("F")] = 0;
-        
-        foreach ($days as $day)
-        {
-            $year = $day->format("Y");
-            $calendar['years'][$year]++;
-            
-            $month = $day->format("F");
-            $calendar['months'][$month]++; 
-            
-            $date = $day->format("Y-m-d");
-            $calendar['dates'][$date]['dayweek'] = $day->format("D");
-            $calendar['dates'][$date]['daymonth'] = $day->format("d");
-            $calendar['dates'][$date]['capacity'] = $daysweek[$day->format("N")] ? $capacity : 0;
-        }
-
-        return $calendar;
-    }
 
 }
 
--- src/PFCD/TourismBundle/Repository/SessionRepository.php
+++ src/PFCD/TourismBundle/Repository/SessionRepository.php
@@ -4,6 +4,13 @@ namespace PFCD\TourismBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
 
+use PFCD\TourismBundle\Entity\Session;
+use PFCD\TourismBundle\Entity\Reservation;
+
+use \DateTime;
+use \DatePeriod;
+use \DateInterval;
+
 class SessionRepository extends EntityRepository
 {
     
@@ -67,6 +74,75 @@ class SessionRepository extends EntityRepository
         // var_dump($qb->getQuery()->getSQL());
 
         return $qb->getQuery()->getResult();
+    }
+    
+    const DATE_FORMAT = "Y-m-d";
+
+    public function findAvailability($activity, $year, $month)
+    {
+        // get the first and last day of the month
+        $dateBeg = new DateTime($year.'-'.$month.'-1');
+        $dateEnd = new DateTime($dateBeg->format('Y-m-t'));
+        
+        // adjust the interval to full weeks from Monday to Sunday
+        if ($dateBeg->format('N') != 1)
+        {
+            $dateBeg = new DateTime(date(self::DATE_FORMAT, strtotime('previous monday', $dateBeg->getTimestamp())));
+        }
+        if ($dateEnd->format('N') != 7)
+        {
+            $dateEnd = new DateTime(date(self::DATE_FORMAT, strtotime('next sunday', $dateEnd->getTimestamp())));
+        }
+        
+        // get the sessions and reservations of the selected interval of dates
+        
+        $dateEnd->setTime(23, 59, 59);
+        $interval = DateInterval::createFromDateString('1 day');
+        $dates = new DatePeriod($dateBeg, $interval, $dateEnd);
+        
+        $calendar = array();
+        
+        foreach ($dates as $date)
+        {
+            $calendar[$date->format("Y-m-d")]['date'] = $date;
+        }
+        
+        $status_session = array(Session::STATUS_ENABLED, Session::STATUS_LOCKED);
+        $status_reservation = array(Reservation::STATUS_REQUESTED, Reservation::STATUS_ACCEPTED);
+        
+        $qb = $this->createQueryBuilder('s');
+        $qb->select('s, SUM(r.persons)');
+        $qb->where('s.activity = :activity');
+        $qb->leftJoin('s.reservations', 'r', 'WITH', 'r.status IN (:status_r)');
+        $qb->andWhere('s.date >= :date_start');
+        $qb->andWhere('s.date <= :date_end');
+        $qb->andWhere('s.status IN (:status_s)');
+        $qb->groupBy('s.id');
+        
+        $qb->setParameter('activity', $activity);
+        $qb->setParameter('status_r', $status_reservation);
+        $qb->setParameter('date_start', $dateBeg);
+        $qb->setParameter('date_end', $dateEnd);
+        $qb->setParameter('status_s', $status_session);
+
+        $result = $qb->getQuery()->getResult();
+        
+        foreach ($result as $row)
+        {
+            $session = $row[0];
+            $id = $session->getId();
+            $date = $session->getDate()->format("Y-m-d");
+            $time = $session->getTime() !== null ? $session->getTime()->format("H:i") : 'undefined';
+            $capacity = $session->getActivity()->getCapacity();
+            $persons = $row[1] ? $row[1] : 0;
+            
+            $calendar[$date]['sessions'][$time]['id'] = $id;
+            $calendar[$date]['sessions'][$time]['time'] = $time;
+            $calendar[$date]['sessions'][$time]['capacity'] = $capacity - $persons;
+            $calendar[$date]['sessions'][$time]['status'] = 1;
+        }
+        
+        return $calendar;
     }
     
 }
