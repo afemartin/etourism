76aa6acc0503961b24ab274c3caee2197ad5d236
Andres Martin <andresmartin8787@gmail.com>
2012-07-13 18:57:27 +0200

    change location for activity_calendar from Back folder to Ajax folder since this template is used both at Front and Back

--- src/PFCD/TourismBundle/Controller/ActivityController.php
+++ src/PFCD/TourismBundle/Controller/ActivityController.php
@@ -19,7 +19,7 @@ class ActivityController extends Controller
 {
 
     /**************************************************************************
-     ***** BACK AREA **********************************************************
+     ***** BACK ACTIONS *******************************************************
      **************************************************************************/
     
     /**
@@ -133,52 +133,6 @@ class ActivityController extends Controller
         
         return $this->render('PFCDTourismBundle:Back/Activity:preview.html.twig', array(
             'activity' => $activity
-        ));
-    }
-    
-    /**
-     * Finds and displays a Activity calendar of sessions
-     */
-    public function backCalendarAction()
-    {
-        $id = $this->get('request')->query->get('id');
-        $year = $this->get('request')->query->get('year');
-        $month = $this->get('request')->query->get('month');
-        
-        $filter['id'] = $id;
-                
-        if ($this->get('security.context')->isGranted('ROLE_ORGANIZATION'))
-        {
-            $filter['organization'] = $this->get('security.context')->getToken()->getUser()->getId();
-        }
-        
-        $em = $this->getDoctrine()->getEntityManager();
-
-        $activity = $em->getRepository('PFCDTourismBundle:Activity')->findOneBy($filter);
-
-        if (!$activity) throw $this->createNotFoundException('Unable to find Activity entity.');
-        
-        $prev = array();
-        $prev['year'] = $month == 1 ? $year - 1 : $year;
-        $prev['month'] = $month == 1 ? 12 : $month - 1;
-        
-        $next = array();
-        $next['year'] = $month == 12 ? $year + 1 : $year;
-        $next['month'] = $month == 12 ? 1 : $month + 1;
-
-        $calendar = $em->getRepository('PFCDTourismBundle:Session')->findAvailability($id, $year, $month);
-        
-        $today = new \DateTime();
-        $today->setTime(0,0,0);
-        
-        return $this->render('PFCDTourismBundle:Back/Activity:ajax_calendar.html.twig', array(
-            'year'     => $year,
-            'month'    => $month,
-            'prev'     => $prev,
-            'next'     => $next,
-            'today'    => $today,
-            'activity' => $activity,
-            'calendar' => $calendar
         ));
     }
     
@@ -343,7 +297,7 @@ class ActivityController extends Controller
     
     
     /**************************************************************************
-     ***** FRONT AREA *********************************************************
+     ***** FRONT ACTIONS ******************************************************
      **************************************************************************/
    
     /**
@@ -376,6 +330,57 @@ class ActivityController extends Controller
         
         return $this->render('PFCDTourismBundle:Front/Activity:read.html.twig', array(
             'activity' => $activity
+        ));
+    }
+        
+    
+    /**************************************************************************
+     ***** COMMON ACTIONS *****************************************************
+     **************************************************************************/
+    
+    /**
+     * Finds and displays a Activity calendar of sessions
+     */
+    public function calendarAction()
+    {
+        $id = $this->get('request')->query->get('id');
+        $year = $this->get('request')->query->get('year');
+        $month = $this->get('request')->query->get('month');
+        
+        $filter['id'] = $id;
+                
+        if ($this->get('security.context')->isGranted('ROLE_ORGANIZATION'))
+        {
+            $filter['organization'] = $this->get('security.context')->getToken()->getUser()->getId();
+        }
+        
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $activity = $em->getRepository('PFCDTourismBundle:Activity')->findOneBy($filter);
+
+        if (!$activity) throw $this->createNotFoundException('Unable to find Activity entity.');
+        
+        $prev = array();
+        $prev['year'] = $month == 1 ? $year - 1 : $year;
+        $prev['month'] = $month == 1 ? 12 : $month - 1;
+        
+        $next = array();
+        $next['year'] = $month == 12 ? $year + 1 : $year;
+        $next['month'] = $month == 12 ? 1 : $month + 1;
+
+        $calendar = $em->getRepository('PFCDTourismBundle:Session')->findAvailability($id, $year, $month);
+        
+        $today = new \DateTime();
+        $today->setTime(0,0,0);
+        
+        return $this->render('PFCDTourismBundle:Ajax:activity_calendar.html.twig', array(
+            'year'     => $year,
+            'month'    => $month,
+            'prev'     => $prev,
+            'next'     => $next,
+            'today'    => $today,
+            'activity' => $activity,
+            'calendar' => $calendar
         ));
     }
     
