dd55c516e27acab1a1fadeb8adc95da1db02efb2
Andres Martin <andresmartin8787@gmail.com>
2012-07-31 03:29:29 +0200

    deleted duplicated line (typo)

--- src/PFCD/TourismBundle/Controller/UserController.php
+++ src/PFCD/TourismBundle/Controller/UserController.php
@@ -185,7 +185,6 @@ class UserController extends Controller
                 $template = $user->getLocale() == 'es' ? 'activation.es.txt.twig' : 'activation.en.txt.twig';
                 $message = \Swift_Message::newInstance()
                         ->setSubject('[' . $this->container->getParameter('pfcd_tourism.domain_name') . '] ' . $this->get('translator')->trans('email.accountactivation.subject'))
-                        ->setSubject('[CooperationTourism] Activation of your account')
                         ->setFrom($this->container->getParameter('pfcd_tourism.emails.no_reply_email'))
                         ->setTo($user->getEmail())
                         ->setBody($this->renderView('PFCDTourismBundle:Mail:' . $template, array('user' => $user)));
