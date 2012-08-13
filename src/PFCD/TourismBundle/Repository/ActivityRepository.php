f516f5e5e49347fd0ab994a63a23070f1bae7b63
Andres Martin <andresmartin8787@gmail.com>
2012-08-13 18:49:59 +0200

    added repository function to get enabled (or locked) activities but only from enabled (or locked organizations) also with a filter for country

--- src/PFCD/TourismBundle/Repository/ActivityRepository.php
+++ src/PFCD/TourismBundle/Repository/ActivityRepository.php
@@ -4,16 +4,39 @@ namespace PFCD\TourismBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
 
-use PFCD\TourismBundle\Entity\Session;
-use PFCD\TourismBundle\Entity\Reservation;
-
-use \DateTime;
-use \DatePeriod;
-use \DateInterval;
+use PFCD\TourismBundle\Entity\Activity;
+use PFCD\TourismBundle\Entity\Organization;
 
 class ActivityRepository extends EntityRepository
 {
 
+    public function findListFront($organization = null, $country = null)
+    {
+        $qb = $this->createQueryBuilder('a');
+        
+        $qb->select('a');
+        $qb->innerJoin('a.organization', 'o');
+                
+        if ($organization !== null)
+        {
+            $qb->where('a.organization = :organization');
+            $qb->setParameter('organization', $organization);
+        }
+        
+        if ($country !== null)
+        {
+            $qb->andWhere('o.country = :country');
+            $qb->setParameter('country', $country);
+        }
+        
+        $qb->andWhere('a.status IN (:status_act)');
+        $qb->setParameter('status_act', array(Activity::STATUS_ENABLED, Activity::STATUS_LOCKED));
+        
+        $qb->andWhere('o.status IN (:status_org)');
+        $qb->setParameter('status_org', array(Organization::STATUS_ENABLED, Organization::STATUS_LOCKED));
+
+        return $qb->getQuery()->getResult();
+    }
 
 }
 
